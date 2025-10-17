06647d282eac7d77c184fafaf355720be4db7e32
Claire Kwong <clairebkwong@gmail.com>
2025-10-17 17:54:07 -0400

    refactor: move convertToTensor into tfHelper

--- src/main.ts
+++ src/main.ts
@@ -61,6 +61,7 @@ const MLMode = {
  * Tensorflow declarations *
  ***************************/
 
+// TODO: Why can I declare tf twice?
 declare const tf: any;
 declare const tfvis: any;
 
@@ -215,7 +216,7 @@ export async function run(
   tfvis.show.modelSummary({ name: "Model Summary" }, model);
 
   // Convert the data to a form we can use for training.
-  const tensorData = convertToTensor(data);
+  const tensorData = tfHelper.convertToTensor(data);
   const { inputs, labels } = tensorData;
 
   // Train the model
@@ -223,51 +224,6 @@ export async function run(
 
   // Return the trained model and normalization data to callers.
   return { model, tensorData };
-}
-
-/**
- * Convert the input data to tensors that we can use for machine
- * learning. We will also do the important best practices of _shuffling_
- * the data and _normalizing_ the data
- * MPG on the y-axis.
- */
-function convertToTensor(data: PoseDatum[]): tfHelper.NormalizationData {
-  // Wrapping these calculations in a tidy will dispose any
-  // intermediate tensors.
-  return tf.tidy(() => {
-    // Step 1. Shuffle the data
-    tf.util.shuffle(data);
-
-    // Step 2. Convert data to Tensor
-    const inputs = data.map((d: PoseDatum) => d.person1Pose);
-    const labels = data.map((d: PoseDatum) => d.person2Pose);
-
-    const inputTensor = tf.tensor2d(inputs, [inputs.length, 66]);
-    const labelTensor = tf.tensor2d(labels, [labels.length, 66]);
-
-    // Step 3. Normalize the data to the range 0 - 1 using min-max scaling
-    const inputMax = inputTensor.max();
-    const inputMin = inputTensor.min();
-    const labelMax = labelTensor.max();
-    const labelMin = labelTensor.min();
-
-    const normalizedInputs = inputTensor
-      .sub(inputMin)
-      .div(inputMax.sub(inputMin));
-    const normalizedLabels = labelTensor
-      .sub(labelMin)
-      .div(labelMax.sub(labelMin));
-
-    return {
-      inputs: normalizedInputs,
-      labels: normalizedLabels,
-      // Return the min/max bounds so we can use them later.
-      inputMax,
-      inputMin,
-      labelMax,
-      labelMin,
-    };
-  });
 }
 
 async function trainModel(model: any, inputs: any, labels: any) {
--- src/utils/tfHelper.ts
+++ src/utils/tfHelper.ts
@@ -57,6 +57,51 @@ export function createModel() {
   return model;
 }
 
+/**
+ * Convert the input data to tensors that we can use for machine
+ * learning. We will also do the important best practices of _shuffling_
+ * the data and _normalizing_ the data
+ * MPG on the y-axis.
+ */
+export function convertToTensor(data: PoseDatum[]): tfHelper.NormalizationData {
+  // Wrapping these calculations in a tidy will dispose any
+  // intermediate tensors.
+  return tf.tidy(() => {
+    // Step 1. Shuffle the data
+    tf.util.shuffle(data);
+
+    // Step 2. Convert data to Tensor
+    const inputs = data.map((d: PoseDatum) => d.person1Pose);
+    const labels = data.map((d: PoseDatum) => d.person2Pose);
+
+    const inputTensor = tf.tensor2d(inputs, [inputs.length, 66]);
+    const labelTensor = tf.tensor2d(labels, [labels.length, 66]);
+
+    // Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+    const inputMax = inputTensor.max();
+    const inputMin = inputTensor.min();
+    const labelMax = labelTensor.max();
+    const labelMin = labelTensor.min();
+
+    const normalizedInputs = inputTensor
+      .sub(inputMin)
+      .div(inputMax.sub(inputMin));
+    const normalizedLabels = labelTensor
+      .sub(labelMin)
+      .div(labelMax.sub(labelMin));
+
+    return {
+      inputs: normalizedInputs,
+      labels: normalizedLabels,
+      // Return the min/max bounds so we can use them later.
+      inputMax,
+      inputMin,
+      labelMax,
+      labelMin,
+    };
+  });
+}
+
 export function renderScatterplot(
   tfvis: any,
   values: { x: number; y: number }[]
